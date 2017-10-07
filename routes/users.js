af9a069d0542b5ef2193a91ab5b3cdb13a74836e
emilyliza <emilyliza@gmail.com>
2017-10-07 08:51:44 -0700

    getting things up and running

--- routes/users.js
+++ routes/users.js
@@ -14,11 +14,11 @@ router.get('/api', function(req, res) {
   console.log("we are in the get request");
   res.render('index');
   // var fullname = req.param('firstname');
-  // var email = req.param('email');
-  // var username = req.param('username');
-  // // var lastname = req.param('lastname');
-  //
-  // res.send(firstname + ' ' + email + ' ' + username);
+  var email = req.param('email');
+  var username = req.param('username');
+  // var lastname = req.param('lastname');
+
+  res.send(firstname + ' ' + email + ' ' + username);
 });
 
 router.post('/api', function(req, res) {
