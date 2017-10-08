6193aacc6bd807dccedf6a022542fe69bf57b09d
emilyliza <emilyliza@gmail.com>
2017-10-08 05:44:46 -0700

    save api push data to db

--- routes/users.js
+++ routes/users.js
@@ -20,26 +20,14 @@ router.post('/adduser', function(req, res) {
     });
 });
 
-router.get('/api', function(req, res) {
-
-    res.json({name: "Emily"});
-
-  // console.log("we are in the get request");
-  // res.render('index');
-  // // var fullname = req.param('firstname');
-  // var email = req.param('email') || "fred@gmail.com";
-  // var username = req.param('username') || "fred";
-  // console.log(email, username);
-  // // res.send(email, username);
-  // var lastname = req.param('lastname');
-
-  // res.send(firstname + ' ' + email + ' ' + username);
-});
-
 router.post('/api', function(req, res) {
-    console.log(req.body);
-
-    res.json(req.body);
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.insert(req.body, function(err, result){
+      res.send(
+          (err === null) ? { msg: '' } : { msg: err }
+      );
+  });
 });
 
 module.exports = router;
