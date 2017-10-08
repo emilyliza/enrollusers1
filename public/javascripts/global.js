1d32ab95d6c241a1e19e55a5dc5da52dee3587c2
emilyliza <emilyliza@gmail.com>
2017-10-08 06:26:49 -0700

    add ability to delete users

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -13,6 +13,9 @@ $(document).ready(function() {
     // Add User button click
    $('#btnAddUser').on('click', addUser);
 
+    // Delete User link click
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+
 });
 
 // Fill table with data
@@ -29,7 +32,8 @@ function populateTable() {
         $.each(data, function(){
             tableContent += '<tr>';
             tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
+            tableContent += '<td>' + this.firstname + ' ' + this.lastname + '</td>';
+            tableContent += '<td>' + this.cohort + '</td>';
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
         });
@@ -54,10 +58,10 @@ function showUserInfo(event) {
     var thisUserObject = userListData[arrayPosition];
 
     //Populate Info Box
-    $('#userInfoName').text(thisUserObject.fullname);
-    $('#userInfoAge').text(thisUserObject.age);
-    $('#userInfoGender').text(thisUserObject.gender);
-    $('#userInfoLocation').text(thisUserObject.location);
+    $('#userInfoName').text(thisUserObject.firstname + '' + thisUserObject.lastname);
+    $('#userInfoUsername').text(thisUserObject.username);
+    $('#userInfoCohort').text(thisUserObject.cohort);
+    $('#userInfoCountry').text(thisUserObject.country);
 
 };
 // Add User
@@ -75,12 +79,13 @@ function addUser(event) {
 
         // If it is, compile all user info into one object
         var newUser = {
-            'username': $('#addUser fieldset input#inputUserName').val(),
+            'username': $('#addUser fieldset input#inputEmail').val(),
             'email': $('#addUser fieldset input#inputUserEmail').val(),
-            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
-            'age': $('#addUser fieldset input#inputUserAge').val(),
-            'location': $('#addUser fieldset input#inputUserLocation').val(),
-            'gender': $('#addUser fieldset input#inputUserGender').val()
+            'firstname': $('#addUser fieldset input#inputUserFirstname').val(),
+            'lastname': $('#addUser fieldset input#inputUserLastname').val(),
+            'country': $('#addUser fieldset input#inputUserCountry').val(),
+            'cohort': $('#addUser fieldset input#inputUserCohort').val(),
+
         }
 
         // Use AJAX to post the object to our adduser service
@@ -114,4 +119,42 @@ function addUser(event) {
         alert('Please fill in all fields');
         return false;
     }
+};
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
 };
--- routes/users.js
+++ routes/users.js
@@ -30,4 +30,14 @@ router.post('/api', function(req, res) {
   });
 });
 
+
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
 module.exports = router;
